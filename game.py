a57752b9c7592aceb3f33023bcb15b87b2d3b1ff
tboussar <thombouss@hotmail.com>
2018-07-10 09:00:53 +0200

    Day-1 change compare algo

--- game.py
+++ game.py
@@ -21,22 +21,16 @@ class Game:
     def __is_game_running(self):
         return self.player_score < self.max_score \
             and self.computer_score < self.max_score
-    def __compare_hands(self, player_hand, computer_hand):
-        if player_hand == computer_hand:
-            print("Draw!")
-        elif any([
-            player_hand == "ROCK" and computer_hand == "SCISSORS",
-            player_hand == "PAPER" and computer_hand == "ROCK",
-            player_hand == "SCISSORS" and computer_hand == "PAPER"
-            ]):
-            self.player_score += 1
-            print("You win this one")
-        # elif (1 + self.HANDS.index(player_hand)) % 3 < (1 + self.HANDS.index(computer_hand)) % 3:
-        #     self.player_score +=1
-        #     print("You win this one")
-        else:
-            self.computer_score += 1
-            print("You lose this one")
+	def __compare_hands(self, player_hand, computer_hand):
+		delta = (3 + self.HANDS.index(computer_hand) - self.HANDS.index(player_hand)) % 3
+		if delta == 0:
+			print("Draw!")
+		elif delta == 1:
+			self.player_score += 1
+			print("You win this one")
+		else:
+			self.computer_score += 1
+			print("You lose this one")
     def __print_score(self):
         print(f"Player score: {self.player_score} - Computer score: {self.computer_score}")
     def __print_game_over(self):
